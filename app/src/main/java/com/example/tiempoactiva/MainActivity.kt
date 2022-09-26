8b7d4f854d0240f270f253e6b505c38a444b87b9
dam2a <amartinezpineiro@danielcastelao.org>
2022-09-26 09:25:59 +0200

    Logcat registra el tiempo activa

--- app/src/main/java/com/example/tiempoactiva/MainActivity.kt
+++ app/src/main/java/com/example/tiempoactiva/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.tiempoactiva
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    var start= System.nanoTime()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("Estado", "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Estado","onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Estado","onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        val time =System.nanoTime() - start/1000000
+        Log.d("Estado","onPause $time")
     }
 }
